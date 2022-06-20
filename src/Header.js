d050f69876a3bd5e84722384fae755a636bbf0db
Jaehyun(Jay) Shim <jaehyunshim85@gmail.com>
2022-06-19 22:08:47 -0600

    added Header component

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <>
+      <h1 className="heading__title--main">GOT ANY PLANS?</h1>
+      <h2 className="heading__title--sub">Plan your week!</h2>
+    </>
+  )
+}
+
+export default Header;
